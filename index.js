b630111cc0f3d9eb9413aa9b28de8f42b32c8942
Germán Gabriel <101677477+GermanGab1781@users.noreply.github.com>
2023-01-12 22:21:18 -0300

    Forgot origin for github pages

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require('./db')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000","https://germangab1781.github.io/"],
   methods:["GET", "POST","UPDATE"],
   credentials:true
 }));
